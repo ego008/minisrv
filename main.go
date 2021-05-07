9c9430e4879820a79b084ce6dffca272f43b4554
ego008 <ego008@gmail.com>
2021-05-07 19:20:29 +0800

    Update main.go

--- main.go
+++ main.go
@@ -5,16 +5,33 @@ import (
 	"github.com/ego008/sdb"
 	"log"
 	"net/http"
+	"time"
 )
 
+var db *sdb.DB
+
 func main() {
+	var err error
+	db, err = sdb.Open("lcdb", nil)
+	if err != nil {
+		log.Panicln(err)
+	}
+
 	http.HandleFunc("/", HelloServer)
-	err := http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Println(err)
 	}
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-	_, _ = fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:]+" "+sdb.B2s([]byte("test")))
+	var ts string
+	key := []byte("create_time")
+	if rs := db.Hget("kv", key); rs.OK() {
+		ts = rs.String()
+	} else {
+		ts = time.Now().String()
+		_ = db.Hset("kv", key, sdb.S2b(ts))
+	}
+	_, _ = fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:]+" "+sdb.B2s([]byte("test"))+" key created:"+ts)
 }
